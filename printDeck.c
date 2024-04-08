a5a9c94f727c48dad7b221d66d751ae5fc228e0a
DiazN1 <161499229+DiazN1@users.noreply.github.com>
2024-04-08 13:29:42 -0700

    Update printDeck.c

--- printDeck.c
+++ printDeck.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include "card.h"
-void printDeck(Card* deck, int numCards, int topDeck) {
+void printDeck(Card* deck, int numCards, int topDeck){
     /**********************************************************************************
     *Print statment below was to track number of cards and top of deck when testing code
     *printf("calling printDeck for %d #cards where topDeck=%d\n", numCards, topDeck);
     ***********************************************************************************/ 
     int i; //counter for where we are in the structure
     //Creates entire deck
-    for (i = topDeck; i < (numCards + topDeck); i++) {
+    for (i = topDeck; i < (numCards + topDeck); i++){
         //creates heart cards
-        if (deck[i].suit == 56) {
+        if (deck[i].suit == 56){
             if (deck[i].value <= 10 && deck[i].value != 1)
                 printf("♥  %d\n", deck[i].value);
             else if (deck[i].value == 11)
@@ -24,7 +24,7 @@ void printDeck(Card* deck, int numCards, int topDeck) {
                 printf("♥  A\n");
         }
         //creates spade cards
-        if (deck[i].suit == 57) {
+        if (deck[i].suit == 57){
             if (deck[i].value <= 10 && deck[i].value != 1)
                 printf("♠  %d\n", deck[i].value);
             else if (deck[i].value == 11)
@@ -37,7 +37,7 @@ void printDeck(Card* deck, int numCards, int topDeck) {
                 printf("♠  A\n");
         }
         //creates club cards
-        if (deck[i].suit == 58) {
+        if (deck[i].suit == 58){
             if (deck[i].value <= 10 && deck[i].value != 1)
                 printf("♣  %d\n", deck[i].value);
             else if (deck[i].value == 11)
@@ -50,7 +50,7 @@ void printDeck(Card* deck, int numCards, int topDeck) {
                 printf("♣  A\n");
         }
         //creates diamond cards
-        if (deck[i].suit == 59) {
+        if (deck[i].suit == 59){
             if (deck[i].value <= 10 && deck[i].value != 1)
                 printf("◆  %d\n", deck[i].value);
             else if (deck[i].value == 11)
