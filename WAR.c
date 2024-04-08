45cf304fc058b8ba7ac87cb37040c31a783dca0c
DiazN1 <161499229+DiazN1@users.noreply.github.com>
2024-04-08 13:34:36 -0700

    Update WAR.c

--- WAR.c
+++ WAR.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/***************************************************************************************
+* This creates the cpus hand and the users hand for as many cards as we want randomly
+* spliting that said amount between the two players then running the comparison function
+* to essencially run the main part of the game.
+****************************************************************************************/
 int main(){
     srand(time(NULL));
     int i;
     int cardsPerHand = 4;
-    Deck cardStack[52];
-    Deck cpuStack[5000];
-    Deck userStack[5000];
+    Card cardStack[52];
+    Card cpuStack[5000];
+    Card userStack[5000];
     for (i = 0; i < 52; i++) {
         int suit = i / 13;
         int value = i % 13;
