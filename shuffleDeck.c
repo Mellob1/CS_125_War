1e4a86cc049d57c633d193c3d4499544b73fdd7b
DiazN1 <161499229+DiazN1@users.noreply.github.com>
2024-04-08 13:31:05 -0700

    Update shuffleDeck.c

--- shuffleDeck.c
+++ shuffleDeck.c
@@ -3,10 +3,10 @@
 #include <time.h>
 #include "card.h"
 //Shuffling Deck of Cards using Fisher Yates Algorithm
-void shuffleDeck(Deck* deck) {
-    int i, j;
-    Deck temp;
-    for (i = CARDS_IN_DECK - 1; i >= 0; i--) {  //HM i changed to i>=0 so first card gets shuffled
+void shuffleDeck(Card* deck){
+    int i, j; //counter variables
+    Card temp;
+    for (i = CARDS_IN_DECK - 1; i >= 0; i--){
         j = rand() % (i + 1);
         temp = deck[i];
         deck[i] = deck[j];
